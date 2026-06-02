77a2c82e17df8bbb51c03fb292a00ec170348275
Irina <menshovaiv@gmail.com>
2026-06-03 00:25:05 +0300

    refactor LoginPageTest

--- src/test/java/by/av/ui/LoginPageTest.java
+++ src/test/java/by/av/ui/LoginPageTest.java
@@ -1,21 +1,30 @@
 package by.av.ui;
 
+import by.av.ui.page.HomePage;
 import by.av.ui.page.LoginPage;
-import by.av.ui.page.RegistrationPage;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class LoginPageTest extends BaseTest {
+	HomePage homePage;
+	LoginPage loginPage;
+
+	@BeforeEach
+	@Override
+	public void setup() {
+		homePage = new HomePage();
+		homePage.open();
+		homePage.clickLoginButton();
+		loginPage = new LoginPage();
+	}
+
 	@DisplayName("Check login with valid credentials")
 	@Test
 	public void testLoginWithValidCredentials() {
-		String emailOrLogin = "tt4999241@gmail.com";
-		String password = "test@1000";
-
-		homePage.clickLoginButton();
-
-		LoginPage loginPage = new LoginPage();
+		String emailOrLogin = "test@gmail.com";
+		String password = "test@109900";
 
 		loginPage.clickLoginByEmailButton();
 		loginPage.setEmailOrLoginInput(emailOrLogin);

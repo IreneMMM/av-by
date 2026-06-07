6ba02710a7617568951087eb8880824ee613a045
Irina Menshova <menshovaiv@gmail.com>
2026-06-08 00:03:53 +0300

    add LoginPageTests + locators; refactor LoginPage + tests

--- src/main/java/by/av/ui/page/LoginPage.java
+++ src/main/java/by/av/ui/page/LoginPage.java
@@ -1,6 +1,7 @@
 package by.av.ui.page;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class LoginPage extends BasePage {
@@ -9,33 +10,65 @@ public class LoginPage extends BasePage {
 	private final By emailOrLoginInput = By.xpath("//input[@id=\"authLogin\"]");
 	private final By passwordInput = By.xpath("//input[@id=\"loginPassword\"]");
 	private final By showPasswordButton = By.xpath("//label[@for=\"loginPassword\"]/button");
-	private final By forgetPasswordButton = By.xpath("//button[@class=\"button button--link button--small\"]");
+	private final By forgetPasswordButton = By.xpath("//button[contains(text(), \"Не помню пароль\")]");
 	private final By submitButton = By.xpath("//button[@class=\"button button--action\"]");
 	private final By errorMessage = By.xpath("//div[@class=\"error-message\"]");
 	private final By userMenu = By.xpath("//ul[@class=\"nav__personal\"]");
+	private final By recoveryPasswordTitle = By.xpath("//div[contains(text(),\"Запрос на восстановление пароля\")]");
+	private final By recoveryPasswordSubmitButton = By.xpath("//div[@aria-labelledby=\"почте\"]//button[contains(text(),\"Отправить\")]");
+	private final By recoveryPasswordEmailInput = By.xpath("//input[@id=\"email\"]");
+	private final By recoveryPasswordByEmailButton = By.xpath("//div[contains(@class, 'drawer__slide--active')]//button[text()='почте']");
 
 	public LoginPage() {
 		super();
+	}
+
+	public WebElement getSubmitButton() {
+		return wait.until(ExpectedConditions.visibilityOfElementLocated(submitButton));
+	}
+
+	public WebElement getRecoveryPasswordSubmitButton() {
+		return wait.until(ExpectedConditions.visibilityOfElementLocated(recoveryPasswordSubmitButton));
 	}
 
 	public void clickLoginByEmailButton() {
 		wait.until(ExpectedConditions.elementToBeClickable(loginByEmailButton)).click();
 	}
 
+	public void clickRecoveryPasswordByEmailButton() {
+		wait.until(ExpectedConditions.elementToBeClickable(recoveryPasswordByEmailButton)).click();
+	}
+
 	public void setEmailOrLoginInput(String email) {
-		driver.findElement(emailOrLoginInput).sendKeys(email);
+		wait.until(ExpectedConditions.elementToBeClickable(emailOrLoginInput)).sendKeys(email);
 	}
 
 	public void setPasswordInput(String pass) {
 		driver.findElement(passwordInput).sendKeys(pass);
 	}
 
+	public void setRecoveryPasswordEmailInput(String email) {
+		driver.findElement(recoveryPasswordEmailInput).sendKeys(email);
+	}
+
 	public void clickSubmitButton() {
 		driver.findElement(submitButton).click();
 	}
 
+	public void clickRecoveryPasswordSubmitButton() {
+		driver.findElement(recoveryPasswordSubmitButton).click();
+	}
+
+	public void clearEmailOrLoginInput() {
+		wait.until(ExpectedConditions.elementToBeClickable(emailOrLoginInput)).clear();
+	}
+
+	public void clearPasswordInput() {
+		wait.until(ExpectedConditions.elementToBeClickable(passwordInput)).clear();
+	}
+
 	public void clickForgetPasswordButton() {
-		driver.findElement(forgetPasswordButton).click();
+		wait.until(ExpectedConditions.elementToBeClickable(forgetPasswordButton)).click();
 	}
 
 	public void clickShowPasswordButton() {
@@ -46,7 +79,7 @@ public class LoginPage extends BasePage {
 		return wait.until(ExpectedConditions.visibilityOfElementLocated(errorMessage)).getText();
 	}
 
-	public boolean isUserMenuDisplayed() {
-		return wait.until(ExpectedConditions.visibilityOfElementLocated(userMenu)).isDisplayed();
+	public String getRecoveryPasswordTitle() {
+		return wait.until(ExpectedConditions.visibilityOfElementLocated(recoveryPasswordTitle)).getText().trim();
 	}
 }
--- src/test/java/by/av/ui/LoginPageTest.java
+++ src/test/java/by/av/ui/LoginPageTest.java
@@ -7,12 +7,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class LoginPageTest extends BaseTest {
 	HomePage homePage;
 	LoginPage loginPage;
 
 	@BeforeEach
-	@Override
 	public void setup() {
 		homePage = new HomePage();
 		homePage.open();
@@ -20,10 +21,24 @@ public class LoginPageTest extends BaseTest {
 		loginPage = new LoginPage();
 	}
 
-	@DisplayName("Check login with valid credentials")
+	@DisplayName("Check submit button is enabled with valid credentials")
 	@Test
-	public void testLoginWithValidCredentials() {
+	public void testSubmitButtonIsEnabledWithValidCredentials() {
 		String emailOrLogin = "test@gmail.com";
+		String password = "test@109900";
+
+		loginPage.clickLoginByEmailButton();
+		loginPage.setEmailOrLoginInput(emailOrLogin);
+		loginPage.setPasswordInput(password);
+
+		Assertions.assertTrue(loginPage.getSubmitButton().isEnabled());
+	}
+
+
+	@DisplayName("Check login with invalid email or login")
+	@Test
+	public void testLoginWithInvalidEmailOrLogin() {
+		String emailOrLogin = "testgmail.com";
 		String password = "test@109900";
 
 		loginPage.clickLoginByEmailButton();
@@ -31,6 +46,82 @@ public class LoginPageTest extends BaseTest {
 		loginPage.setPasswordInput(password);
 		loginPage.clickSubmitButton();
 
-		Assertions.assertTrue(loginPage.isUserMenuDisplayed(), "User menu is not displayed");
+		String actualErrorMessage = loginPage.getErrorMessage();
+		String expectedErrorMessage = "Неверный логин или пароль. Если забыли пароль, восстановите его";
+
+		Assertions.assertEquals(expectedErrorMessage,actualErrorMessage);
+	}
+
+	@DisplayName("Check login with invalid password")
+	@Test
+	public void testLoginWithInvalidPassword() {
+		String emailOrLogin = "test@gmail.com";
+		String password = "t12123";
+
+		loginPage.clickLoginByEmailButton();
+		loginPage.setEmailOrLoginInput(emailOrLogin);
+		loginPage.setPasswordInput(password);
+		loginPage.clickSubmitButton();
+
+		String actualErrorMessage = loginPage.getErrorMessage();
+		String expectedErrorMessage = "Неверный логин или пароль. Если забыли пароль, восстановите его";
+
+		Assertions.assertEquals(expectedErrorMessage,actualErrorMessage);
+	}
+
+	@DisplayName("Check submit button is disabled with empty credentials")
+	@Test
+	public void testSubmitButtonIsDisableWithEmptyCredentials() {
+		loginPage.clickLoginByEmailButton();
+		loginPage.clearEmailOrLoginInput();
+		loginPage.clearPasswordInput();
+		Assertions.assertFalse(loginPage.getSubmitButton().isEnabled());
+	}
+
+	@DisplayName("Check submit button is disabled with empty email or login")
+	@Test
+	public void testSubmitButtonIsDisableWithEmptyEmailOrLogin() {
+		String password = "t12123";
+
+		loginPage.clickLoginByEmailButton();
+		loginPage.clearEmailOrLoginInput();
+		loginPage.setPasswordInput(password);
+		Assertions.assertFalse(loginPage.getSubmitButton().isEnabled());
+	}
+
+	@DisplayName("Check submit button is disabled with empty password")
+	@Test
+	public void testSubmitButtonIsDisableWithEmptyPassword() {
+		String emailOrLogin = "dfg";
+
+		loginPage.clickLoginByEmailButton();
+		loginPage.setEmailOrLoginInput(emailOrLogin);
+		loginPage.clearPasswordInput();
+		Assertions.assertFalse(loginPage.getSubmitButton().isEnabled());
+	}
+
+	@DisplayName("Check forget password button")
+	@Test
+	public void testForgetPasswordButton() {
+		loginPage.clickLoginByEmailButton();
+		loginPage.clickForgetPasswordButton();
+
+		String actualTitle = loginPage.getRecoveryPasswordTitle();
+		String expectedTitle = "Запрос на восстановление пароля";
+		assertAll(
+				() -> assertEquals(expectedTitle, actualTitle),
+				() -> assertFalse(loginPage.getRecoveryPasswordSubmitButton().isEnabled())
+		);
+	}
+
+	@DisplayName("Check recovery password submit button is enabled")
+	@Test
+	public void testRecoveryPasswordSubmitButtonIsEnabled() {
+		String email = "test@gmail.com";
+		loginPage.clickLoginByEmailButton();
+		loginPage.clickForgetPasswordButton();
+		loginPage.clickRecoveryPasswordByEmailButton();
+		loginPage.setRecoveryPasswordEmailInput(email);
+		Assertions.assertTrue(loginPage.getRecoveryPasswordSubmitButton().isEnabled());
 	}
 }
